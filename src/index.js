1be06e779826573d3c43458440a1bd703bf2d6a2
Marcy Silverman <marcysilve225@gmail.com>
2019-02-15 15:08:57 -0800

    rendered a node to the dom

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const title = React.createElement(
+  'h1',
+  { id: 'main-title', title: 'This is a title.' },
+  'My First React Element!'
+);
+
+const desc = React.createElement(
+  'p',
+  null,
+  'I just learned how to create a React node to render in the DOM'
+);
+
+const header = React.createElement(
+  'header',
+  null,
+  title, 
+  desc
+  
+)
+ReactDOM.render(
+  header, 
+  document.getElementById('root')
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
