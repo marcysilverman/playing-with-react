3ab2703533d3c77b41c20fda6921eb2a735ddae0
Marcy Silverman <marcysilve225@gmail.com>
2019-02-15 23:01:08 -0800

    increment score is working

--- src/index.js
+++ src/index.js
@@ -2,25 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+// import { ReactComponent } from '*.svg';
 
 const players = [
   {
     name: "Marcy",
-    score: 50
+    score: 50,
+    id: 1
   },
   {
     name: "Treasure",
-    score: 85
+    score: 85,
+    id: 2
   },
   {
     name: "Ashley",
-    score: 95
+    score: 95,
+    id: 3
   },
   {
     name: "James",
-    score: 80
+    score: 80,
+    id: 4
   }
-]
+];
+
 const Header = (props) => {
   return (
     <header>
@@ -30,25 +36,38 @@ const Header = (props) => {
   )
 };
 
-const Player = (props) => {
-  return (
-    <div className="player">
-    <span className="player-name">
-      { props.name }
-    </span>
-    <Counter score={props.score}/>
-    </div>
-  )
+class Player extends React.Component {
+  render() {
+    return (
+      <div className="player">
+      <span className="player-name">
+        { this.props.name }
+      </span>
+      <Counter score={ this.props.score}/>
+      </div>
+    )
+  }
 };
 
-const Counter = (props) => {
-  return (
-    <div className="counter">
-    <button className="counter-action decrement"> - </button>
-    <button className="counter-score"> { props.score} </button>
-    <button className="counter-action increment"> + </button>
-    </div>
-  )
+class Counter extends React.Component {
+  state= {
+      score: 0
+  };
+
+  incrementScore() {
+    this.setState({
+      score: this.state.score + 1
+    });
+  };
+  render() {
+    return (
+      <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <button className="counter-score"> { this.state.score} </button>
+      <button className="counter-action increment" onClick={() => this.incrementScore()}> + </button>
+      </div>
+    )
+  } 
 };
 
 const App = (props) => {
@@ -59,7 +78,7 @@ const App = (props) => {
       {props.initialPlayers.map(player =>
          <Player 
          name={player.name}
-         score={player.score} 
+         key={player.id.toString()}
       /> )}
     </div>
   )
