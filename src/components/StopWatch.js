ce1282124d7cf23e78e134215697128f1fd1b2ac
Marcy Silverman <marcysilve225@gmail.com>
2019-02-18 11:24:29 -0800

    able to reset the timer

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
 
 class StopWatch extends Component {
+  state = {
+    isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0
+  };
+  
+  componentDidMount(){
+    this.intervalID = setInterval(()=> this.tick(), 100);
+  }
+
+  tick = () => {
+   if(this.state.isRunning) {
+     const now = Date.now();
+     this.setState( prevState => ({
+      previousTime: now,
+      elapsedTime: prevState.elapsedTime + (now - this.state.previousTime)
+     }));
+   }
+  }
+
+  handleStopWatch = () => {
+    this.setState( prevState => ({
+      isRunning: !prevState.isRunning
+    }));
+    if (!this.state.isRunning) {
+      this.setState({ previousTime: Date.now() });
+    }
+  }
+  
+  handleReset = () => {
+    this.setState({ elapsedTime: 0 })
+  }
   render() {
     return (
       <div className="stopwatch">
       <h2>Stopwatch</h2>
-      <span className="stopwatch-time"> 0 </span>
-      <button>Start</button>
-      <button> Reset </button>
+      <span className="stopwatch-time"> { Math.floor(this.state.elapsedTime / 1000) } </span>
+      <button onClick={this.handleStopWatch}>
+      { this.state.isRunning ? 'Stop': 'Start'}
+      </button>
+      <button onClick={this.handleReset}> Reset</button>
       </div>
     );
   }
